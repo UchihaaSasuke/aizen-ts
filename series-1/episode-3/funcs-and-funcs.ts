682d4238ac736ca71ebd10646e3e752ab7917682
UchihaaSasuke <cdrevision@gmail.com>
2022-12-11 14:06:46 +0100

    episode 3- Typescript Functions with Functions

---
+++ series-1/episode-3/funcs-and-funcs.ts
@@ -0,0 +1,25 @@
+export function printToFile(text: string, callback: () => void): void {
+  console.log(text);
+  callback();
+}
+
+export type MutationFunction = (v: number) => number;
+
+export function arrayMutate(
+  numbers: number[],
+  mutate: MutationFunction
+): number[] {
+  return numbers.map(mutate);
+}
+
+console.log(arrayMutate([1, 2, 3], (v) => v * 10));
+
+// returning function
+
+export type AdderFunction = (val: number) => number;
+export function createAdder(num: number): AdderFunction {
+  return (val: number) => num + val;
+}
+
+const addOne = createAdder(1);
+console.log(addOne(11));
